b34cdef3695f06f1cda7804dd96dbe74ae0e3af8
Mert Oksuz <xrefresh@gmail.com>
2016-06-12 14:41:39 +0300

    Security firewall bug fix, ApiDoc annotation tested

--- src/RestApi/ApiBundle/Controller/DefaultController.php
+++ src/RestApi/ApiBundle/Controller/DefaultController.php
@@ -3,14 +3,25 @@
 namespace RestApi\ApiBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class DefaultController extends FOSRestController
 {
+    /**
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Gets Api Home",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the page is not found"
+     *   }
+     * )
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function getHomesAction()
     {
         $data = array("hello" => "world");
         $view = $this->view($data);
-        var_dump($view); exit;
         return $this->handleView($view);
     }
 }
